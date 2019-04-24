f2a3e42e1d6b8ae3b73303d6f44f19feaaf3dcf2
gugu <goncalo.horacarvalho@gmail.com>
2019-04-24 16:37:42 +0200

    Skeleton

---
+++ mathTopicAday.java
@@ -0,0 +1,27 @@
+/* 
+ * what's popin
+ */
+public class Math {
+    // The public constants
+    // public static final double DEFAULT_Var = 7;
+    
+    // The private instance variables
+    // private double priv_Var;
+    
+    // The (overloaded) constructors
+  /*public Circle() {                    1st (default) Constructor
+       this.Var = DEFAULT_Var;
+  */
+
+    // The public getters and setters for the private variables
+  /*  public double getVar() {
+       return this.Var;
+    }
+    public void setVar(double Var) {
+       this.Var = radius;
+  */
+    // The toString() returns a String description of this instance
+    public String toString() {
+       return "Topic[var=" + Var +"]";
+    }
+ }
